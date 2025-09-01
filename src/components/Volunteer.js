1c60bb8b2237bb745067da51bdeae5e88011438f
talaosman <talaosman180@gmail.com>
2025-09-01 15:53:16 +0300

    Modify

--- src/components/Volunteer.js
+++ src/components/Volunteer.js
@@ -51,7 +51,7 @@ const Volunteer = () => (
     <h2>Volunteer & Extracurricular</h2>
     <div className="cards"> 
       <div className="card">
-        <h3>الأمير مجيدارسلان مؤسسة </h3>
+        <h3> مؤسسةالأمير مجيدارسلان  </h3>
         <p>Inputting & organizing client data to provide free medical care</p>
         <p>05/2022 – 03/2022, Choueifat, Lebanon</p>
       </div>
