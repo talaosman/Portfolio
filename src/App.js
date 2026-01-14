e248633a4ffbf5b82585bab5b3b48f6dda4621ae
Tala osman <talaosman180@gmail.com>
2026-01-14 10:45:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
             <div className="loading-bar">
               <div className="progress"></div>
             </div>
-            <p className="loading-text">Loading ...</p>
+            <p className="loading-text">Loading...</p>
           </div>
         </div>
       ) : (
